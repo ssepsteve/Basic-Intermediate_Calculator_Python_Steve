52abfdf5cef6ea3876a8745c06233b79ff83aeeb
_ssep_ <sebastianstve@gmail.com>
2022-05-09 21:46:16 -0500

    Habia Copiado Mal
    Copie el archivo que no era asi que se corrigio

--- BasicFundaments.py
+++ BasicFundaments.py
@@ -7,23 +7,27 @@ def Button1():
     else:
         text_calc = text_calc + "1"
 def Button2():
+    global text_calc
     if text_calc == "0":
         text_calc = "2"
     else:
         text_calc = text_calc + "2"
-def Button3(text_calc):
+def Button3():
+    global text_calc
     if text_calc == "0":
         text_calc = "3"
     else:
         text_calc = text_calc + "3"
-    return text_calc
-def Button4(text_calc):
+    
+def Button4():
+    global text_calc
     if text_calc == "0":
         text_calc = "4"
     else:
         text_calc = text_calc + "4"
-    return text_calc
+    
 def Button5():
+    global text_calc
     if text_calc == "0":
         text_calc = "5"
     else:
@@ -35,16 +39,19 @@ def Button6():
     else:
         text_calc = text_calc + "6"
 def Button7():
+    global text_calc
     if text_calc == "0":
         text_calc = "7"
     else:
         text_calc = text_calc + "7"
 def Button8():
+    global text_calc
     if text_calc == "0":
         text_calc = "8"
     else:
         text_calc = text_calc + "8"
 def Button9():
+    global text_calc
     if text_calc == "0":
         text_calc = "6"
     else:
@@ -56,10 +63,12 @@ def Button0():
 def Add():
     global text_calc
     text_calc = text_calc + "+"
-    
-Button1()
-Button0()
-Add()
+
+Button5()
 Button6()
+Add()
+Button4()
+Button8()
+
 result = eval(text_calc)
 print(result)
