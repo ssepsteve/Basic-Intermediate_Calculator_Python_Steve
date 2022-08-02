bf04ef5273eff377cc6d3b4a2b0c85b8fa4ac20b
_ssep_ <sebastianstve@gmail.com>
2022-08-01 21:50:57 -0500

    opBar, result y euler
    opBar: cree otra variable de la clase la cual tiene como funcion ser lo que se desplegara en pantalla, a diferencia de text_calc, esta variable contiene los simbolos comunes como π en vez de el valor en decimal, ya no se muestra la multiplicacion con * sino con "x", el problema es que esto se muestra pegado lo cual no se ve tambien, lo que podria hacer es añadirle espacios antes, pero justo al inicio de la funcion de result podria poner una funcion integrada la cual me quite los espacios
    Result: esta funcion tuvo varios cambios ya que ahora no solo almacena el resultado en una variable, si no que se encarga de reemplazar los valores simbolicos como π con su respectivo valor en decimal DENTRO de text_calc, para que luego se use eval en text_calc, ese resultado se almacene en result y ese ultimo dentro de opBar
    Euler: Se integro el numero de euler de la misma manera que el numero de pi solo que esta vez con math.e

--- BasicFundaments.py
+++ BasicFundaments.py
@@ -1,5 +1,5 @@
 import EssentialsLib as Lib1
-
+import math
 
 main = Lib1.Essentials()
 
@@ -9,73 +9,76 @@ while True:
     a = a.lower()
     if a == "add" or a == "+":
         main.Add()
-        print(main.text_calc)
+        print(main.opPlace)
     elif a == "pi":
-        main.piButton(round(Lib1.pi,4))
-        print(main.text_calc)
+        main.piButton("π")
+        print(main.opPlace)
+    elif a == "euler":
+        main.eulerButton("e")
+        print(main.opPlace)
     elif a == "sub" or a == "-":
         main.Sub()
-        print(main.text_calc)
+        print(main.opPlace)
     elif a == "point" or a == ".":
         main.Point()
-        print(main.text_calc)
+        print(main.opPlace)
     elif a == "res" or a == "=" :
-        main.Result()
-        print(main.text_calc)
+        main.Result(round(math.pi,4),round(math.e,4))
+        print(main.opPlace)
     elif a == "exit":
         break     
     elif a == "div" or a == "/":
         main.Div()
-        print(main.text_calc)
+        print(main.opPlace)
     elif a == "(":
         main.Open_Par()
-        print(main.text_calc)   
+        print(main.opPlace)
     elif a == ")":
         main.Close_Par()
-        print(main.text_calc)   
+        print(main.opPlace) 
     elif a == "multi" or a == "*":
         main.Multi()
-        print(main.text_calc)
+        print(main.opPlace)
     elif a == "ac" or a == "del":
         main.AC()
-        print(main.text_calc)
+        print(main.opPlace)
     elif a == "1":
         main.sudoButton(1)
-        print(main.text_calc)
+        print(main.opPlace)
     elif a == "2":
         main.sudoButton(2)
-        print(main.text_calc)
+        print(main.opPlace)
     elif a == "3":
         main.sudoButton(3)
-        print(main.text_calc)
+        print(main.opPlace)
     elif a == "4":
         main.sudoButton(4)
-        print(main.text_calc)
+        print(main.opPlace)
     elif a == "5":
         main.sudoButton(5)
-        print(main.text_calc)
+        print(main.opPlace)
     elif a == "6":
         main.sudoButton(6)
-        print(main.text_calc)
+        print(main.opPlace)
     elif a == "7":
         main.sudoButton(7)
-        print(main.text_calc)
+        print(main.opPlace)
     elif a == "8":
         main.sudoButton(8)
-        print(main.text_calc)
+        print(main.opPlace)
     elif a == "9":
         main.sudoButton(9)
-        print(main.text_calc)
+        print(main.opPlace)
     elif a == "0":
         main.sudoButton(0)
-        print(main.text_calc)
-   
+        print(main.opPlace) 
     elif a == "pow":
         main.powe()
-        print(main.text_calc)
-
+        print(main.opPlace)
     elif a == "square":
         main.square()
-        print(main.text_calc)
-
+        print(main.opPlace)
+    '''elif a == "root":
+        main.root()
+        print(main.opPlace)'''
 print("Program End")
--- EssentialsLib.py
+++ EssentialsLib.py
@@ -1,69 +1,102 @@
-from math import pi
-import math
 class Essentials:
     
     def __init__(self):
         self.result = 0
         self.text_calc = "0"
+        self.opPlace = '0'
         self.pulses = 0
+        self.numbers = {1,2,3,4,5,6,7,8,9}
         
     
     #Botones De Numeros:
     def sudoButton(self,n):
-        if self.text_calc == "0":
-            self.text_calc = str(n)
-        elif self.text_calc.endswith(")"):
-            self.text_calc = self.text_calc+"*"+str(n)
+        if self.opPlace == "0":
+            self.opPlace = str(n)
+        elif self.opPlace.endswith(")"):
+            self.opPlace = self.opPlace+"x"+str(n)
         else:
-            self.text_calc = self.text_calc + str(n)
+            self.opPlace = self.opPlace + str(n)
   
     # Botones De Funciones principales:
 
     def Add(self):
-        self.text_calc
-        self.text_calc = self.text_calc + "+"
+        self.opPlace
+        self.opPlace = self.opPlace + "+"
         self.pulses = 0
 
     def Div(self):
-        self.text_calc
-        self.text_calc = self.text_calc + "/"
+        self.opPlace
+        self.opPlace = self.opPlace + "÷"
         self.pulses = 0
 
     def Multi(self):
-        self.text_calc
-        self.text_calc = self.text_calc + "*"
+        self.opPlace
+        self.opPlace = self.opPlace + "x"
         self.pulses = 0
 
     def AC(self):
         
-        self.text_calc
+        self.opPlace
+        self.opPlace = "0"
         self.text_calc = "0"
+        self.result = 0
 
     def Sub(self):
-        self.text_calc
-        self.text_calc = self.text_calc + "-"
+        self.opPlace
+        self.opPlace = self.opPlace + "-"
         self.pulses = 0
 
-    def Result(self):
+    def Result(self,pi,eu):
+        for i in self.opPlace:
+            if i in str(self.numbers):
+                if self.text_calc == "0":
+                    self.text_calc = str(i)
+                elif self.text_calc.endswith(")"):
+                    self.text_calc = self.text_calc+"*"+str(i)
+                else:
+                    self.text_calc = self.text_calc + str(i)
+            elif i == "π":
+                if self.text_calc == "0":
+                    self.text_calc = str(pi)
+                elif self.text_calc.endswith(")"):
+                    self.text_calc = self.text_calc+"*"+str(pi)
+                else:
+                    self.text_calc = self.text_calc + str(pi)
+            elif i == "e":
+                if self.text_calc == "0":
+                    self.text_calc = str(eu)
+                elif self.text_calc.endswith(")"):
+                    self.text_calc = self.text_calc+"*"+str(pi)
+                else:
+                    self.text_calc = self.text_calc + str(pi)
+            elif i == "x":
+                self.text_calc = self.text_calc + "*"
+            elif i == "÷":
+                self.text_calc = self.text_calc + "/"
+            elif i == "^":
+                self.text_calc = self.text_calc + "**"
+            else:
+                self.text_calc = self.text_calc + str(i)
+
         self.result = eval(self.text_calc)
-        self.text_calc = str(self.result)
+        self.opPlace = str(self.result)
         self.pulses = 0
         
     def Open_Par(self):
         self.pulses = 0
-        if self.text_calc.endswith("-") or self.text_calc.endswith("+"):
-            self.text_calc = self.text_calc + "("
+        if self.opPlace.endswith("-") or self.opPlace.endswith("+"):
+            self.opPlace = self.opPlace + "("
         else:
-            self.text_calc = self.text_calc + "*("
+            self.opPlace = self.opPlace + "x("
         
     def Close_Par(self):
 
-        self.text_calc = self.text_calc + ")"
+        self.opPlace = self.opPlace + ")"
 
     def Point(self):
         
         if self.pulses == 0:
-            self.text_calc = self.text_calc + "."
+            self.opPlace = self.opPlace + "."
             self.pulses = 1
         else:
             pass
@@ -74,18 +107,27 @@ class Essentials:
     '''Cientifica'''
     def piButton(self, x):
         
-        if self.text_calc == "0" :
-            self.text_calc = str(x)
-        elif self.text_calc.endswith("(")or self.text_calc.endswith("+") or self.text_calc.endswith("-") or self.text_calc.endswith("*")or self.text_calc.endswith("/"):
-            self.text_calc = self.text_calc + str(x)
+        if self.opPlace == "0" :
+            self.opPlace = str(x)
+        elif self.opPlace.endswith("(")or self.opPlace.endswith("+") or self.opPlace.endswith("-") or self.opPlace.endswith("*")or self.opPlace.endswith("/"):
+            self.opPlace = self.opPlace + str(x)
         else:
-            self.text_calc = self.text_calc + "*"+str(x)
+            self.opPlace = self.opPlace + "x"+str(x)
+    
+    def eulerButton(self, e):
+        if self.opPlace == "0" :
+            self.opPlace = str(e)
+        elif self.opPlace.endswith("(")or self.opPlace.endswith("+") or self.opPlace.endswith("-") or self.opPlace.endswith("*")or self.opPlace.endswith("/"):
+            self.opPlace = self.opPlace + str(e)
+        else:
+            self.opPlace = self.opPlace + "x"+str(e)
 
     def square(self):
-        self.text_calc = self.text_calc + "**2"
+        self.opPlace = self.opPlace + "^2"
 
     def powe(self):
-        self.text_calc = self.text_calc + "**"
+        self.opPlace = self.opPlace + "^"
     
-    
+    '''def root(self):
+        self.opPlace = self.opPlace + "√"'''
    
